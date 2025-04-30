c172319b71790fa92d2e7278df8b30cff11ceef0
PeterF13456 <5006569@stu.highlineschools.org>
2025-04-30 13:46:16 -0700

    score stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour { 
 
     public AudioSource theMusic;
@@ -8,11 +8,26 @@ public class GameManager : MonoBehaviour {
 
     public BeatScroller theBS;
 
+    public static GameManager instance;
+
+    public int currentScore;
+    public int scorePerNote = 100;
+
+    public Text scoreText;
+    public Text multiText;
+
+    public int currentMultiplier;
+    public int multiplierTracker;
+    public int[] multiplierThresholds;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
+        instance = this;
+
+        scoreText.text = "Score: 0";
+        currentMultiplier = 1;
+      }
 
     // Update is called once per frame
     void Update()
@@ -27,5 +42,36 @@ public class GameManager : MonoBehaviour {
                 theMusic.Play();
             }
         }
+    }
+
+
+    public void NoteHit()
+    {
+        Debug.Log("Hit On Time");
+
+        if (currentMultiplier - 1 < multiplierThresholds.Length)
+        {
+            multiplierTracker++;
+
+            if (multiplierThresholds[currentMultiplier - 1] <= multiplierTracker)
+            {
+                multiplierTracker = 0;
+                currentMultiplier++;
+            }
+        }
+
+        multiText.text = "Multiplier: X" + currentMultiplier;
+
+        currentScore += scorePerNote * currentMultiplier;
+        scoreText.text = "Score: " + currentScore;
+    }
+    public void NoteMissed()
+    {
+        Debug.Log("Missed Note");
+
+       // currentMultiplier = 1;
+     //   multiplierTracker = 0;
+
+      //  multiText.text = "Multiplier: X" + currentMultiplier;
     }
 }
