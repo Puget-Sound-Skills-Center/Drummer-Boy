051ee097fa999316fe87689deac567cf9b2adf46
Liam-Archer <5006697@stu.highlineschools.org>
2025-05-01 13:33:49 -0700

    pause menu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    public KeyCode KeyToPress;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    
+     
+            AudioSource[] audios = FindObjectsOfType<AudioSource>();
+
+        foreach (AudioSource a in audios)
+        {
+            a.Pause();
+        }
+
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene("Main Menu");
+        Time.timeScale = 1;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+      
+            AudioSource[] audios = FindObjectsOfType<AudioSource>();
+
+
+            foreach (AudioSource a in audios)
+            {
+                a.Play();
+            }
+
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+
+    public void Update()
+    {
+      
+    }
+}
